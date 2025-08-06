27508e06fbab0026480440a178a97609ede16220
zizoumunoz <zmunoz@outlook.es>
2025-08-06 14:07:23 -0700

    Moved ColorARGB docstrings to header

--- Star Traveller C++/ColorARGB.cpp
+++ Star Traveller C++/ColorARGB.cpp
@@ -1,9 +1,6 @@
 #include "ColorARGB.h"
 #include <iostream>
 
-/// <summary>
-/// Creates transparent black color.
-/// </summary>
 ColorARGB::ColorARGB()
 {
 	_alpha = 0;
@@ -17,17 +14,6 @@ ColorARGB::ColorARGB(int alpha, int red, int green, int blue)
 {
 }
 
-/// <summary>
-/// Retrieves the specified ARGB component value from the ColorARGB object.
-/// </summary>
-/// <param name="value">
-/// (char) A character indicating which component to retrieve:
-/// 'A' or 'a' for alpha, 'R' or 'r' for red, 'G' or 'g' for green, 'B' or 'b' for blue.
-/// </param>
-/// <returns>
-/// The integer value of the requested ARGB component.
-/// Returns 0 if the input is invalid and logs an error to std::cerr.
-/// </returns>
 int ColorARGB::getValue(char value) const
 {
 	switch (value)
--- Star Traveller C++/ColorARGB.h
+++ Star Traveller C++/ColorARGB.h
@@ -3,11 +3,31 @@
 class ColorARGB
 {
 public:
+	/// <summary>
+	/// Creates default transparent black color.
+	/// </summary>
 	ColorARGB();
+
+	/// <summary>
+	/// Creates color obj with given ARGB values.
+	/// </summary>
 	ColorARGB(int alpha, int red, int green, int blue);
+
+	/// <summary>
+	/// Retrieves the specified ARGB component value from the ColorARGB object.
+	/// </summary>
+	/// <param name="value">
+	/// (char) A character indicating which component to retrieve:
+	/// 'A' or 'a' for alpha, 'R' or 'r' for red, 'G' or 'g' for green, 'B' or 'b' for blue.
+	/// </param>
+	/// <returns>
+	/// The integer value of the requested ARGB component.
+	/// Returns 0 if the input is invalid and logs an error to std::cerr.
+	/// </returns>
 	int getValue(char value) const;
 
-	friend class AsciiHandler;
+	// For argb value access when displaying
+	friend class AsciiHandler;	
 
 private:
 	int _alpha;
